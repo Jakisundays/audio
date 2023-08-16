e044817ec531ad82d6f1252413a99925d981cf9c
Jakisundays <jakisundays@gmail.com>
2023-08-16 15:07:45 -0300

    Optimización de Procesamiento de Audio

--- app/api/split-and-convert/route.ts
+++ app/api/split-and-convert/route.ts
@@ -1,70 +1,90 @@
 import Ffmpeg from "fluent-ffmpeg";
 import { Readable } from "stream";
 
-export async function POST(request: Request) {
+// Función asincrónica para procesar un fragmento de audio
+const processChunk = async (chunkBlob: Blob, i: number): Promise<string> => {
+  // Obtener el ArrayBuffer del fragmento de audio
+  const audioArrayBuffer = await chunkBlob.arrayBuffer();
+  const outputFileName = `audio_${i}.mp3`; // Nombre del archivo de salida
+
+  return new Promise((resolve, reject) => {
+    // Crear un flujo legible a partir del ArrayBuffer
+    const audioStream = new Readable({
+      read() {
+        const buffer = Buffer.from(audioArrayBuffer);
+        this.push(buffer);
+        this.push(null);
+      },
+    });
+
+    // Procesar el fragmento de audio utilizando FFmpeg
+    Ffmpeg(audioStream)
+      .output(outputFileName)
+      .format("mp3")
+      .on("start", (commandLine: any) => {
+        console.log("FFmpeg iniciado con el comando: " + commandLine);
+      })
+      .on("error", (err: any) => {
+        console.log("Se produjo un error: " + err.message);
+        reject(err);
+      })
+      .on("end", () => {
+        console.log(`Procesamiento finalizado para ${outputFileName}`);
+        resolve(outputFileName);
+      })
+      .run();
+  });
+};
+
+// Función asincrónica para manejar la solicitud POST
+export const POST = async (request: Request): Promise<Response> => {
+  // Obtener los datos del formulario de la solicitud
   const formData = await request.formData();
   const audioFile = formData.get("audio");
 
-  if (audioFile instanceof Blob) {
-    const cuantosMB: number = 2;
-    const CHUNK_SIZE = cuantosMB * 1024 * 1024; // Tamaño del fragmento en bytes (por ejemplo, 2 MB) 1024 = valor binario
-    let start = 0;
-    let end = Math.min(CHUNK_SIZE, audioFile.size);
-    const chunkBlobs = [];
-
-    // Dividir el archivo de audio en fragmentos
-    while (start < audioFile.size) {
-      const chunkBlob = new Blob([audioFile.slice(start, end)], {
-        type: audioFile.type,
-      });
-      chunkBlobs.push(chunkBlob);
-
-      // Preparar para el siguiente fragmento
-      start = end;
-      end = Math.min(start + CHUNK_SIZE, audioFile.size);
-    }
-    console.log({ chunkBlobs });
-
-    // Iterar a través de cada fragmento y procesarlos uno por uno
-    for (let i = 0; i < chunkBlobs.length; i++) {
-      const chunkBlob = chunkBlobs[i];
-      const audioArrayBuffer = await chunkBlob.arrayBuffer();
-      const outputFileName = `audio_${i}.mp3`; // Establecer el nombre de archivo de salida
-
-      // Convertir ArrayBuffer a un flujo legible
-      const audioStream = new Readable({
-        read() {
-          // Convertir el ArrayBuffer en un búfer
-          const buffer = Buffer.from(audioArrayBuffer);
-          this.push(buffer);
-          this.push(null);
-        },
-      });
-
-      // Procesar el fragmento con FFmpeg
-      Ffmpeg(audioStream)
-        .output(outputFileName) // Establecer el nombre del archivo de salida
-        .format("mp3")
-        .on("start", function (commandLine: any) {
-          console.log("FFmpeg iniciado con el comando: " + commandLine);
-        })
-        .on("error", function (err: any) {
-          console.log("Se produjo un error: " + err.message);
-        })
-        .on("end", function () {
-          console.log("Procesamiento finalizado");
-        })
-        .run();
-    }
-    // Devolver una respuesta con información sobre chunkBlobs (puedes modificar esto si es necesario)
-    return new Response(JSON.stringify(chunkBlobs), {
+  // Verificar si se proporcionó un archivo de audio válido
+  if (!(audioFile instanceof Blob)) {
+    return new Response(JSON.stringify({ message: "No se proporcionó un archivo de audio válido." }), {
+      status: 400,
       headers: {
         "Content-Type": "application/json",
       },
     });
-  } else {
-    return new Response("No se proporcionó un archivo de audio válido.", {
-      status: 400,
+  }
+
+  // Configurar el tamaño del fragmento y dividir el archivo de audio en fragmentos
+  const cuantosMB: number = 2;
+  const CHUNK_SIZE = cuantosMB * 1024 * 1024;
+  const chunkBlobs: Blob[] = [];
+  let start = 0;
+
+  while (start < audioFile.size) {
+    const end = Math.min(start + CHUNK_SIZE, audioFile.size);
+    const chunkBlob = new Blob([audioFile.slice(start, end)], {
+      type: audioFile.type,
+    });
+    chunkBlobs.push(chunkBlob);
+    start = end;
+  }
+  console.log(`Total de fragmentos: ${chunkBlobs.length}`);
+
+  try {
+    // Procesar todos los fragmentos en paralelo y obtener los nombres de archivo procesados
+    const processedFiles = await Promise.all(chunkBlobs.map(processChunk));
+    console.log("Procesamiento completado con éxito");
+    return new Response(JSON.stringify({ processedFiles }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.log("Se produjo un error durante el procesamiento:", error);
+    return new Response(JSON.stringify({ message: "Se produjo un error durante el procesamiento." }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
   }
-}
+};
